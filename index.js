b7861e1cdf2b3c422131ceac8e5d19bcc3e1f5fd
Faiz Abbas <faiz_abbas@hotmail.co.uk>
2018-11-06 16:28:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function shout(string) {
    return string.toUpperCase();
 }
+
+function whispe(string) {
+  return string.toLowerCase();
+}
+
+function longShout(string){
+  console.log(string.toUpperCase());
+}
